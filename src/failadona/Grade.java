7b01df1c86b25b39389398eddebbdc1a205a353a
Failadona <danielfailadona6@gmail.com>
2024-08-14 12:12:30 +0800

    this is my first repo

---
+++ src/failadona/Grade.java
@@ -0,0 +1,11 @@
+package failadona;
+
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+        Grade grd = new Grades();
+        grd.getgrade
+    }
+    
+}
