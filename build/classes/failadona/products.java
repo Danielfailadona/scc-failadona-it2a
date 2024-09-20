c2ba8c360ae24f266e1f6093e8194ecc5888e8ca
Danielfailadona <danielfailadona6@gmail.com>
2024-09-20 00:25:02 -0700

    Add files via upload

---
+++ build/classes/failadona/products.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationnew;
+
+/**
+ *
+ * @author PC009
+ */
+class products {
+
+    void viewProduct() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addProduct(int id, String name, int st, double pri, int sold) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
