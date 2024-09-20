2dd59cf552d5ed6201bd2b5fc01c28fb00db732e
Danielfailadona <danielfailadona6@gmail.com>
2024-09-20 00:04:03 -0700

    PRELIM

---
+++ Account.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC009
+ */
+class Account {
+
+    Account(String firstName, String lastName, String email, String username, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
